fddd368bf098262f648a969ef44e863624918b70
tu <tu.jabberwocky2@gmail.com>
2023-05-29 20:11:11 +0900

    Update add cors setting

--- api/c#/icon-kun-api-proto/WebApplication1/Program.cs
+++ api/c#/icon-kun-api-proto/WebApplication1/Program.cs
@@ -1,4 +1,16 @@
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000",
+                                              "http://localhost:7265");
+                      });
+});
 
 // Add services to the container.
 
@@ -15,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
