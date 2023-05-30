a8d876e6470c5267e28bba48d8e88e5973e2c073
tu <tu.jabberwocky2@gmail.com>
2023-05-30 09:38:08 +0900

    fix format

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bulma/css/bulma.css';
-import App from './App';
-import Header from './components/modules/Header';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import 'bulma/css/bulma.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+import Header from './components/modules/Header'
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
