97f859337f64cda6f734c789ba6ae4df10c0260b
tu <tu.jabberwocky2@gmail.com>
2023-05-30 09:14:06 +0900

    Add component imageUploader

---
+++ web/src/components/modules/ImageUploder.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ImageUploder() {
+    return (
+        <div>
+            <input type='file' accept='image/*' alt='alt' title='image' />
+            <div>
+                <img src="" title='image' />
+                TEXT TEXT TEXT TEXT TEXT TEXT 
+            </div>
+        </div>
+    );
+}
+
+export default ImageUploder;
