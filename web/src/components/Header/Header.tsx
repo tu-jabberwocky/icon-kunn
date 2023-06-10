8a9be97378ae957495557183bd2151e9c430fde3
tu <tu.jabberwocky2@gmail.com>
2023-06-10 10:24:39 +0900

    format

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import logo from './image/image_FILL0_wght400_GRAD0_opsz48.svg'
+import logo from './image/image_FILL0_wght400_GRAD0_opsz48.svg';
 
 function Header() {
-    return (
-        <header>
-            <section className='hero is-info'>
-                <div className='hero-body columns'>
-                    <div className='column is-2'>
-                        <a className='nabvar-item' href=''>
-                            <img src={logo} alt="logo" />
-                        </a>
-                    </div>
-                    <div className='colmns'>
-                        <h1 className='title'>icon-kunn</h1>
-                        <h1 className='sybtitle'>画像からアイコンを生成します。</h1>
-                    </div>
-                </div>
-            </section>
-        </header>
-    );
+  return (
+    <header>
+      <section className="hero is-info">
+        <div className="hero-body columns">
+          <div className="column is-2">
+            <a className="nabvar-item" href="">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+          <div className="colmns">
+            <h1 className="title">icon-kunn</h1>
+            <h1 className="sybtitle">画像からアイコンを生成します。</h1>
+          </div>
+        </div>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
