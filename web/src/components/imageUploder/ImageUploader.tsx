16140187cc1187ff14136f5e168a27cb8dcd65cc
tu <tu.jabberwocky2@gmail.com>
2023-06-10 14:02:23 +0900

    Refactor

--- web/src/components/imageUploder/ImageUploader.tsx
+++ web/src/components/imageUploder/ImageUploader.tsx
@@ -7,30 +7,6 @@ import React, { useRef, useState } from 'react';
 import RectCoordinate from '../../types/RectCoordinate';
 import Canvas from '../imageUploder/canvas/Canvas';
 
-interface CoordinateInfoProps {
-  coordinate: RectCoordinate;
-  onCoordinatesUpdated: (coordinate: RectCoordinate) => void;
-}
-
-const CoordinateInfo: React.FC<CoordinateInfoProps> = ({
-  coordinate,
-  onCoordinatesUpdated,
-}) => {
-  const handleClick = () => {
-    onCoordinatesUpdated(coordinate);
-  };
-
-  return (
-    <div className="coordinate-info">
-      <span>Left: {coordinate.x}</span>
-      <span>Top: {coordinate.y}</span>
-      <span>Width: {coordinate.width}</span>
-      <span>Height: {coordinate.height}</span>
-      <button onClick={handleClick}>Copy Coordinates</button>
-    </div>
-  );
-};
-
 interface ImageUploaderProps {
   onCoordinatesUpdated: (coordinate: RectCoordinate) => void;
 }
@@ -40,10 +16,6 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({
 }) => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  const [coordinate, setCoordinate] = useState<RectCoordinate | null>(null);
-  const imageRef = useRef<HTMLImageElement>(null);
-  const startPositionRef = useRef({ x: 0, y: 0 });
-  const isSelectingRef = useRef(false);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -55,45 +27,6 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({
       };
       reader.readAsDataURL(file);
     }
-  };
-
-  const handleImageMouseDown = (event: React.MouseEvent<HTMLImageElement>) => {
-    const image = imageRef.current;
-    if (image) {
-      isSelectingRef.current = true;
-      const rect = image.getBoundingClientRect();
-      startPositionRef.current = {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top,
-      };
-    }
-  };
-
-  const handleImageMouseMove = (event: React.MouseEvent<HTMLImageElement>) => {
-    if (isSelectingRef.current) {
-      const image = imageRef.current;
-      if (image) {
-        const rect = image.getBoundingClientRect();
-        const currentX = event.clientX - rect.left;
-        const currentY = event.clientY - rect.top;
-        const width = currentX - startPositionRef.current.x;
-        const height = currentY - startPositionRef.current.y;
-        setCoordinate({
-          x: startPositionRef.current.x,
-          y: startPositionRef.current.y,
-          width,
-          height,
-        });
-      }
-    }
-  };
-
-  const handleImageMouseUp = () => {
-    isSelectingRef.current = false;
-  };
-
-  const handleCoordinatesUpdated = (coordinate: RectCoordinate) => {
-    onCoordinatesUpdated(coordinate);
   };
 
   return (
@@ -116,23 +49,10 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({
         </label>
       </div>
       {previewUrl && (
-        <div className="image-container">
-          <img
-            ref={imageRef}
-            src={previewUrl}
-            alt="Preview"
-            onMouseDown={handleImageMouseDown}
-            onMouseMove={handleImageMouseMove}
-            onMouseUp={handleImageMouseUp}
-          />
-          {coordinate && <Canvas coordinate={coordinate} />}
-          {coordinate && (
-            <CoordinateInfo
-              coordinate={coordinate}
-              onCoordinatesUpdated={handleCoordinatesUpdated}
-            />
-          )}
-        </div>
+        <Canvas
+          imageUrl={previewUrl}
+          onCoordinatesUpdated={onCoordinatesUpdated}
+        />
       )}
     </div>
   );
--- web/src/components/imageUploder/canvas/Canvas.tsx
+++ web/src/components/imageUploder/canvas/Canvas.tsx
@@ -5,11 +5,16 @@ import React, { useEffect, useRef, useState } from 'react';
 import RectCoordinate from '../../../types/RectCoordinate';
 
 interface CanvasProps {
-  coordinate: RectCoordinate;
+  imageUrl: string;
+  onCoordinatesUpdated: (coordinate: RectCoordinate) => void;
 }
 
-const Canvas: React.FC<CanvasProps> = ({ coordinate }) => {
+const Canvas: React.FC<CanvasProps> = ({ imageUrl, onCoordinatesUpdated }) => {
+  const [coordinate, setCoordinate] = useState<RectCoordinate | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const startPositionRef = useRef({ x: 0, y: 0 });
+  const isSelectingRef = useRef(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -17,21 +22,91 @@ const Canvas: React.FC<CanvasProps> = ({ coordinate }) => {
       const ctx = canvas.getContext('2d');
       if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        ctx.fillRect(
+          coordinate ? coordinate.x : 0,
+          coordinate ? coordinate.y : 0,
+          coordinate ? coordinate.width : 0,
+          coordinate ? coordinate.height : 0
+        );
       }
     }
   }, [coordinate]);
 
+  useEffect(() => {
+    const image = new Image();
+    image.src = imageUrl;
+    image.onload = () => {
+      const canvas = canvasRef.current;
+      if (canvas) {
+        // canvas.width = image.width;
+        // canvas.height = image.height;
+      }
+    };
+  }, [imageUrl]);
+
+  const handleImageMouseDown = (event: React.MouseEvent<HTMLImageElement>) => {
+    const image = imageRef.current;
+    if (image) {
+      isSelectingRef.current = true;
+      const rect = image.getBoundingClientRect();
+      startPositionRef.current = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+      };
+    }
+  };
+
+  const handleImageMouseMove = (event: React.MouseEvent<HTMLImageElement>) => {
+    if (isSelectingRef.current) {
+      const image = imageRef.current;
+      if (image) {
+        const rect = image.getBoundingClientRect();
+        const currentX = event.clientX - rect.left;
+        const currentY = event.clientY - rect.top;
+        const width = currentX - startPositionRef.current.x;
+        const height = currentY - startPositionRef.current.y;
+        const newCoordinate: RectCoordinate = {
+          x: startPositionRef.current.x,
+          y: startPositionRef.current.y,
+          width,
+          height,
+        };
+        setCoordinate(newCoordinate);
+        onCoordinatesUpdated(newCoordinate);
+      }
+    }
+  };
+
+  const handleImageMouseUp = () => {
+    isSelectingRef.current = false;
+  };
+
+  const rectStyle = {
+    top: coordinate ? coordinate.y : 0,
+    left: coordinate ? coordinate.x : 0,
+    width: coordinate ? coordinate.width : 0,
+    height: coordinate ? coordinate.height : 0,
+  };
+
   return (
-    <canvas
-      ref={canvasRef}
-      className="selection-overlay"
-      style={{
-        top: coordinate.y,
-        left: coordinate.x,
-        width: coordinate.width,
-        height: coordinate.height,
-      }}
-    />
+    <div className="image-container">
+      <img
+        ref={imageRef}
+        src={imageUrl}
+        alt="Preview"
+        onMouseDown={handleImageMouseDown}
+        onMouseMove={handleImageMouseMove}
+        onMouseUp={handleImageMouseUp}
+      />
+      {coordinate && (
+        <canvas
+          ref={canvasRef}
+          style={rectStyle}
+          className="selection-overlay"
+        />
+      )}
+    </div>
   );
 };
 
